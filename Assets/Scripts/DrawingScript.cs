9fbcff713fc5d4c4b3cf827e7eef3890a2d38dee
qzmp <lukasz.golebiowski@mail.ru>
2015-03-22 19:05:42 +0100

    Soldier stucking fix and gesture drawing fixes
    gesture drawing bugless? and ready for recognition implementation

--- Assets/Scripts/DrawingScript.cs
+++ Assets/Scripts/DrawingScript.cs
@@ -3,49 +3,77 @@ using System.Collections.Generic;
 
 public class DrawingScript : MonoBehaviour {
 
-	private Dictionary<int, GameObject> trails = new Dictionary<int, GameObject>();
+	private Dictionary<int, TrailHistory> trails = new Dictionary<int, TrailHistory>();
 	
 	void Update()
 	{
-		for (int i = 0; i < Input.touchCount; i++) {
-			Touch touch = Input.GetTouch(i);
-
+		foreach(Touch touch in Input.touches) 
+		{
 			// -- Drag
 			// ------------------------------------------------
-			if (touch.phase == TouchPhase.Began) {
+			if (touch.phase == TouchPhase.Began)
+			{
 				// Store this new value
-				if (trails.ContainsKey (i) == false) {
+				if (!trails.ContainsKey(touch.fingerId))
+				{
 					Vector3 position = Camera.main.ScreenToWorldPoint (touch.position);
 					position.z = 0; // Make sure the trail is visible
 
 					GameObject trail = SpecialEffectsScript.MakeTrail (position);
+
 								
-					if (trail != null) {
+					if (trail != null)
+					{
 						Debug.Log (trail);
-						trails.Add (i, trail);
+						TrailHistory trailHistory = new TrailHistory(trail);
+						trails.Add (touch.fingerId, trailHistory);
 					}
 				}
-			} else if (touch.phase == TouchPhase.Moved) {
+			} 
+			else if (touch.phase == TouchPhase.Moved)
+			{
 				// Move the trail
-				if (trails.ContainsKey (i)) {
-					GameObject trail = trails [i];
-							
-					Camera.main.ScreenToWorldPoint (touch.position);
+				if (trails.ContainsKey (touch.fingerId))
+				{
+					TrailHistory trailHistory = trails[touch.fingerId];
+
 					Vector3 position = Camera.main.ScreenToWorldPoint (touch.position);
 					position.z = 0; // Make sure the trail is visible
 							
-					trail.transform.position = position;
+					trailHistory.trail.transform.position = position;
+					trailHistory.addPosition(position);
+
 				}
-			} else if (touch.phase == TouchPhase.Ended) {
+			} 
+			else if (touch.phase == TouchPhase.Ended)
+			{
 				// Clear known trails
-				if (trails.ContainsKey (i)) {
-					GameObject trail = trails [i];
+				if (trails.ContainsKey (touch.fingerId))
+				{
+					TrailHistory trailHistory = trails[touch.fingerId];
 							
 					// Let the trail fade out
-					Destroy (trail, trail.GetComponent<TrailRenderer> ().time);
-					trails.Remove (i);
+					Destroy (trailHistory.trail, trailHistory.trail.GetComponent<TrailRenderer> ().time);
+					trails.Remove (touch.fingerId);
 				}
 			}
+		}
+	}
+
+	private class TrailHistory
+	{
+		public GameObject trail{ get; set; }
+		public List<Vector3> positions{ get; private set;}
+
+		public TrailHistory(GameObject trail)
+		{
+			this.trail = trail;
+			positions = new List<Vector3>();
+		}
+
+		public void addPosition(Vector3 pos)
+		{
+			positions.Add (pos);
 		}
 	}
 }
